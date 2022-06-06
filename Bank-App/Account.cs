1b327aecae4679bad62502ef4bd1fd41e3ac4e0d
devmanagalaxy <54977839+force9493@users.noreply.github.com>
2022-06-06 19:57:58 -0400

    Add structure and Account class

---
+++ Bank-App/Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank_App
+{
+    class Account
+    {
+        private decimal balance = 0;
+
+        public bool withdraw(decimal amount)
+        {
+            if(balance < amount)
+            {
+                return false;
+            }
+            balance -= amount;
+            return true;
+        }
+
+        public bool deposit(decimal amount)
+        {
+            if(amount <= 0)
+            {
+                return false;
+            }
+            balance += amount;
+            return true;
+        }
+
+        public decimal getBalace()
+        {
+            return balance;
+        }
+    }
+}
---
+++ Bank-App/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Bank_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool withdrawRes = false;
+            bool depositRes = false;
+            decimal amount = 0;
+            Account nAccount;
+            nAccount = new Account();
+
+            amount = decimal.Parse(Console.ReadLine());
+            depositRes = nAccount.deposit(amount);
+
+            Console.WriteLine("Deposit " + (depositRes ? "succeed" : "failed"));
+
+            amount = decimal.Parse(Console.ReadLine());
+            withdrawRes = nAccount.withdraw(amount);
+
+            Console.WriteLine("Withdraw " + (withdrawRes ? "succeed" : "failed"));
+
+            Console.WriteLine("Account balance is " + nAccount.getBalace());
+        }
+    }
+}
